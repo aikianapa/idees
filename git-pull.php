3135de291201704cd98ff531b2fd542f64455cd7
Oleg Frolov <oleg_frolov@mail.ru>
2019-08-27 15:59:07 +0300

    изменено:      git-pull.php

--- git-pull.php
+++ git-pull.php
@@ -1,4 +1,4 @@
 <?php
-echo exec("cd ". __DIR__ . " && git pull ");
-echo "<br><hr>Done";
+echo exec('cd ' . __DIR__ . ' && git pull https://aikianapa:aikido78@github.com/aikianapa/idees.git');
+echo "<br><hr>Done ".$ret;
 ?>
