c7b1e251a335f38773f944416a8036124d0b3614
Oleg Frolov <oleg_frolov@mail.ru>
2019-08-27 23:50:26 +0300

    update

---
+++ tpl/404.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en" data-wb-role="formdata" data-wb-form="{{_form}}" data-wb-item="{{_item}}" data-wb-hide="wb">
+<meta data-wb-role="include" data-wb-src="template" data-wb-name="head.inc.php" data-wb-hide="*">
+<body class="preloader-body">
+<meta data-wb-role="include" data-wb-src="template" data-wb-name="loader.inc.php" data-wb-hide="*">
+<meta data-wb-role="include" data-wb-src="template" data-wb-name="header.inc.php" data-wb-hide="*">
+
+
+<div class="top-block company-head">
+    <div class="container-fluid">
+        <div class="top-block__subtitle" data-wb-role="tree" data-wb-item="blogtags" data-wb-branch="{{tag}}" data-wb-hide="wb">
+            Страница, которую вы запрашиваете, была перемещена или удалена.
+        </div>
+        <div class="row">
+            <div class="col-xl-6">
+                <h1 class="top-block__title">
+                    Страница не найдена
+                </h1>
+            </div>
+            <div class="col-xl-6">
+                <div class="company-head__desc">
+                    Ошибка 404
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="post-content" style="background:url(/tpl/img/bg/404.jpg) 90% 0 no-repeat; background-size:cover;">
+    <div class="container-fluid">
+                <div class="post-container" style="background:transparent; height:70vh;">
+                    <div class="post-inner">
+                        <section class="post-section post-section--top">
+                            <div class="row">
+                                <div class="col-md-6 post-left">
+                                    <h2 class="post-h2">Ошибка 404</h2>
+                                    <p>Страница не найдена. Возможно, она была перемещена или удалена. Воспользуйтесь меню для перехода в другой раздел.</p>
+                                </div>
+                                <div class="col-md-6 post-right">
+                                    <div class="post-count">
+                                        <div class="post-count__big">&nbsp;</div>
+                                        <div class="post-count__text">
+                                            &nbsp;
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                    </div>
+                </div>
+</div>
+    
+
+<meta data-wb-selector=".post-section img" data-wb-addclass="img-fluid">
+<meta data-wb-selector=".post-section img" data-wb-removeattr="style">
+<meta data-wb-selector="title" data-wb-html="Ошибка 404">
+<meta data-wb-role="include" data-wb-src="template" data-wb-name="footer.inc.php" data-wb-hide="*">
+</body>
+</html>
