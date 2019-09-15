da111acec90e8b9ed0291cc9107b8a0158f1798e
unknown <ideeshtml@gmail.com>
2019-09-15 17:42:24 +0700

    fix footer:

--- tpl/404.php
+++ tpl/404.php
@@ -6,20 +6,25 @@
 <meta data-wb-role="include" data-wb-src="template" data-wb-name="header.inc.php" data-wb-hide="*">
 
 
-<div class="top-block company-head">
+<div class="top-block company-head page404">
     <div class="container-fluid">
-        <div class="top-block__subtitle" data-wb-role="tree" data-wb-item="blogtags" data-wb-branch="{{tag}}" data-wb-hide="wb">
-            Страница, которую вы запрашиваете, была перемещена или удалена.
-        </div>
-        <div class="row">
-            <div class="col-xl-6">
-                <h1 class="top-block__title">
-                    Страница не найдена
-                </h1>
+        <div class="row align-items-center">
+            <div class="col-xl-6 order-xl-last mb-5 mb-xl-0">
+                <div class="page404__img-wrap text-center text-xl-right">
+                    <img class="page404__img" src="img/svg/404.svg">
+                </div>
             </div>
             <div class="col-xl-6">
-                <div class="company-head__desc">
-                    <img class="success-form__svg" src="img/svg/404.svg">
+                <h1 class="page404__title" data-wb-role="tree" data-wb-item="blogtags" data-wb-branch="{{tag}}" data-wb-hide="wb">
+                    К сожалению эта страница не существует
+                </h1>
+                <div class="page404__text">
+                    Вы ищете страницу, которая была перемещена или удалена. Пожалуйста, перейдите на главную страницу сайта.
+                </div>
+                <div class="text-center text-xl-left">
+                    <a href="/" class="button button--white-outline">
+                        Перейти на главную
+                    </a>
                 </div>
             </div>
         </div>
