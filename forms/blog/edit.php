32d7871a138edb93f1995aa06e4f107bfe855532
Oleg Frolov <oleg_frolov@mail.ru>
2021-12-13 18:01:25 +0300

    update

--- forms/blog/edit.php
+++ forms/blog/edit.php
@@ -5,17 +5,12 @@
         <div class="modal-content">
             <div class="modal-header px-3">
                 <i class="fa fa-close wd-20" data-dismiss="modal" aria-label="Close"></i>
-                <h6>Редактирование проекта</h6>
+                <h6>Редактирование статьи</h6>
                 <div class="">
-                    <div class="custom-control custom-switch d-inline-block mr-3">
-                        <input type="checkbox" class="custom-control-input" name="yonger" id="{{_form}}SwitchItemYonger"
-                            onchange="$('#{{_form}}ValueItemYonger').prop('checked',$(this).prop('checked'));">
-                        <label class="custom-control-label" for="{{_form}}SwitchItemYonger">Yonger</label>
-                    </div>
                     <div class="custom-control custom-switch d-inline-block">
                         <input type="checkbox" class="custom-control-input" name="active" id="{{_form}}SwitchItemActive"
                             onchange="$('#{{_form}}ValueItemActive').prop('checked',$(this).prop('checked'));">
-                        <label class="custom-control-label" for="{{_form}}SwitchItemActive">Активен</label>
+                        <label class="custom-control-label" for="{{_form}}SwitchItemActive">Отображать</label>
                     </div>
                 </div>
             </div>
@@ -25,47 +20,28 @@
                         <form id="{{_form}}EditForm">
                             <input type="checkbox" class="custom-control-input" name="active"
                                 id="{{_form}}ValueItemActive">
-                            <input type="checkbox" class="custom-control-input" name="yonger"
-                                id="{{_form}}ValueItemYonger">
-
                             <div class="form-group row">
-
-        <div class="col-12 mb-2">
-            <input name="cover" wb="module=filepicker&mode=single&width=650&height=215"
-                wb-path="/uploads/projects/cover" class="pt-0">
-        </div>
-                                <div class="col-12">
-                                    <div class="form-group row">
-                                        <div class="col-12">
-                                            <label class="form-control-label">Наименование проекта</label>
-                                            <input type="text" name="name" class="form-control" required>
-                                        </div>
-                                        <div class="col-12">
-                                            <label class="form-control-label">Краткое описание</label>
-                                            <textarea type="text" name="descr" class="form-control" required
-                                                wb="module=langinp"></textarea>
-                                        </div>
-                                    </div>
-                                </div>
-
-                            <div class="col-8">
-                                    <label>Клиент</label>
-                                    <input class="form-control" type="text" name="client">
-                                </div>
-                                <div class="col-4">
-                                    <label>Год реализации</label>
-                                    <input class="form-control" type="text" name="year">
-                                </div>
-
-                                <div class="col-8">
-                                    <label>Ссылка на проект</label>
-                                    <input class="form-control" type="text" name="link">
-                                </div>
-                                <div class="col-4">
-                                    <label>Бюджет</label>
-                                    <input class="form-control" type="text" name="budget">
+                                <div class="col-12 mb-2">
+                                    <input name="cover" wb="module=filepicker&mode=single&width=650&height=215"
+                                        wb-path="/uploads/projects/cover" class="pt-0">
                                 </div>
                             </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="form-group row">
+                                    <div class="col-12">
+                                        <label class="form-control-label">Наименование проекта</label>
+                                        <input type="text" name="name" class="form-control" required>
+                                    </div>
+                                    <div class="col-12">
+                                        <label class="form-control-label">Краткое описание</label>
+                                        <textarea type="text" name="descr" class="form-control" required
+                                            wb="module=langinp"></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                             <div class="form-group row">
                                 <label class="col-sm-2">Разделы</label>
                                 <div class="col-sm-10">
@@ -86,16 +62,16 @@
                             <div class="form-group row">
                                 <label class="col-sm-2">Похожие</label>
                                 <div class="col-sm-10">
-                                        <select name="similar" class="form-control" wb-select2 multiple>
-                                            <wb-foreach wb="table=projects" wb-filter="active=on">
-                                                <option value="{{_id}}" selected
-                                                    wb-if="'{{in_array({{_id}},{{_parent._parent.tags}})}}'=='1'">
-                                                    {{name}}</option>
-                                                <option value="{{_id}}"
-                                                    wb-if="'{{in_array({{_id}},{{_parent._parent.tags}})}}'!='1'">
-                                                    {{name}}</option>
-                                            </wb-foreach>
-                                        </select>
+                                    <select name="similar" class="form-control" wb-select2 multiple>
+                                        <wb-foreach wb="table={{_form}}" wb-filter="active=on">
+                                            <option value="{{_id}}" selected
+                                                wb-if="'{{in_array({{_id}},{{_parent._parent.tags}})}}'=='1'">
+                                                {{name}}</option>
+                                            <option value="{{_id}}"
+                                                wb-if="'{{in_array({{_id}},{{_parent._parent.tags}})}}'!='1'">
+                                                {{name}}</option>
+                                        </wb-foreach>
+                                    </select>
                                 </div>
                             </div>
                             <div class="row">
