534a0ff730b264043d7f82ed1d8ac48f01f972d8
Oleg Frolov <oleg_frolov@mail.ru>
2021-12-30 03:03:40 +0300

    update

--- blocks/sitemenu.php
+++ blocks/sitemenu.php
@@ -1,17 +1,9 @@
-<view>
-  <wb-var menu='[]' />
-  <wb-var header='' />
-  <wb-data wb="table=pages&item=_footer">
-    <wb-foreach wb="from=blocks&tpl=false">
-      <wb-var menu='{{sitemenu.data}}' wb-if="'{{name}}'=='sitemenu'" />
-      <wb-var header='{{_val.header}}' wb-if="'{{name}}'=='sitemenu'" />
+<view><wb-var menu="[]"></wb-var><wb-var header=""></wb-var><wb-data wb="table=pages&amp;item=_footer">
+    <wb-foreach wb="from=blocks&amp;tpl=false">
+      <wb-var menu="{{sitemenu.data}}" wb-if="'{{name}}'=='sitemenu'"></wb-var>
+      <wb-var header="{{_val.header}}" wb-if="'{{name}}'=='sitemenu'"></wb-var>
     </wb-foreach>
-  </wb-data>
-
-
-
-
-  <section class="menu modal">
+  </wb-data><section class="menu modal">
     <header class="header header--menu container">
       <a class="header__logo-link" href="/">
         <svg class="header__logo-image" width="157" height="50" fill="none" viewbox="0 0 157 50">
@@ -40,12 +32,12 @@
       <nav class="menu__navigation">
         <ul class="menu__list">
           <wb-foreach wb="from=_var.menu">
-            <li class="menu__item menu__item--{{id}}">
-              <a class="menu__link" href="{{data.link}}">{{name}}</a>
-              <ul class="menu__sub-list" wb-if="'{{children}}'>''">
+            <li class="menu__item menu__item--{{id}}" wb-if='yongerIsPage("{{data.link}}")'>
+              <a class="menu__link" href="%7B%7Bdata.link%7D%7D">{{name}}</a>
+              <ul class="menu__sub-list" wb-if="'{{children}}'&gt;''">
                 <wb-foreach wb="from=children">
-                  <li class="menu__sub-item">
-                    <a class="menu__sub-link" href="{{data.link}}">{{name}}</a>
+                  <li class="menu__sub-item" wb-if='yongerIsPage("{{data.link}}")'>
+                    <a class="menu__sub-link" href="%7B%7Bdata.link%7D%7D">{{name}}</a>
                   </li>
                 </wb-foreach>
               </ul>
@@ -55,18 +47,18 @@
       </nav>
 
       <div class="menu__contacts">
-        <a class="menu__contact-link" href="email:{{_var.email}}" target="_blank">{{_var.email}}</a>
-        <a class="menu__contact-link" href="tel:+{{wbDigitsOnly({{_var.phone}})}}">{{_var.phone}}</a>
+        <a class="menu__contact-link" href="email:%7B%7B_var.email%7D%7D" target="_blank">{{_var.email}}</a>
+        <a class="menu__contact-link" href="tel:+%7B%7BwbDigitsOnly(%7B%7B_var.phone%7D%7D)%7D%7D">{{_var.phone}}</a>
 
         <ul class="menu__socials-list">
           <li class="menu__socials-item">
-            <a class="menu__socials-link" href="{{_var.whatsapp}}">Whatsapp</a>
+            <a class="menu__socials-link" href="%7B%7B_var.whatsapp%7D%7D">Whatsapp</a>
           </li>
           <li class="menu__socials-item">
-            <a class="menu__socials-link" href="{{_var.telegram}}">Telegram</a>
+            <a class="menu__socials-link" href="%7B%7B_var.telegram%7D%7D">Telegram</a>
           </li>
           <li class="menu__socials-item">
-            <a class="menu__socials-link" href="{{_var.messenger}}">Messenger</a>
+            <a class="menu__socials-link" href="%7B%7B_var.messenger%7D%7D">Messenger</a>
           </li>
         </ul>
       </div>
@@ -75,13 +67,7 @@
     <div class="menu__bottom-panel">
       <button class="menu__accept-button js-form-open" type="button">Начать проект</button>
     </div>
-  </section>
-
-
-</view>
-<edit header="Меню сайта">
-  <div>
-    <wb-module wb="module=yonger&mode=edit&block=common.inc" />
+  </section></view><edit header="Меню сайта"><div>
+    <wb-module wb="module=yonger&amp;mode=edit&amp;block=common.inc"></wb-module>
   </div>
-  <input name="sitemenu" wb-tree>
-</edit>
+<input name="sitemenu" wb-tree></edit>
