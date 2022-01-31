36c1890817330a8214ecda37c6f1051147462cf0
Oleg Frolov <oleg_frolov@mail.ru>
2022-01-31 16:15:55 +0300

    comparsion

--- blocks/project-block.php
+++ blocks/project-block.php
@@ -8,10 +8,10 @@
 
 
         <img-comparison-slider wb-if="'{{before.0.img}}'>'' AND '{{after.0.img}}'>''">
-            <img class="project__content-image" width="928" height="600" data-src="/thumbc/928x600/src{{before.0.img}}" data-srcset="/thumbc/1856x1200/src{{before.0.img}} 2x" alt="{{alt}}"
+            <img class="project__content-image" width="928" height="600" src="/thumbc/928x600/src{{before.0.img}}" srcset="/thumbc/1856x1200/src{{before.0.img}} 2x" alt="{{alt}}"
                 slot="first">
 
-            <img class="project__content-image" width="928" height="600" data-src="/thumbc/928x600/src{{after.0.img}}" data-srcset="/thumbc/1856x1200/src{{before.0.img}} 2x"
+            <img class="project__content-image" width="928" height="600" src="/thumbc/928x600/src{{after.0.img}}" srcset="/thumbc/1856x1200/src{{before.0.img}} 2x"
                 alt="{{alt}}" slot="second">
         </img-comparison-slider>
 
