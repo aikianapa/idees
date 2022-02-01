2de423a04e702a8371822c9ebc62f6dd65f9a2f3
Oleg Frolov <oleg_frolov@mail.ru>
2022-02-01 14:39:55 +0300

    update

--- blocks/services.php
+++ blocks/services.php
@@ -1,22 +1,27 @@
 <view>
-    <section class="services">
+    <section class="services container">
         <div class="page__title-container">
             <h1 class="page__title">{{title}}</h1>
         </div>
 
         <ol class="services__list">
-            <wb-foreach wb="from=list&tpl=false">
-                <li class="services__item">
+            <wb-foreach wb="table=pages&tpl=false" wb-filter="{
+                'active':'on',
+                'path':'\/services'
+            }">
+                <wb-data wb="field=blocks.block_paragraph">
+                <li class="services__item" wb-if="'{{active}}'=='on'">
                     <h2 class="services__item-title">
-                        <a class="services__item-link" href="{{blk_title_link}}">{{blk_title}}</a>
+                        <a class="services__item-link" href="{{_parent.url}}" wb-if="'{{title}}'==''">{{_parent.header}}</a>
+                        <a class="services__item-link" href="{{_parent.url}}" wb-if="'{{title}}'>''">{{title}}</a>
                     </h2>
 
-                    <p class="services__item-description">{{blk_text}}</p>
+                    <p class="services__item-description">{{text}}</p>
 
                     <ul class="services__sub-list">
-                        <wb-foreach wb="from=links&tpl=false">
+                        <wb-foreach wb="table={{_parent.attach}}&tpl=false" wb-filter="{{_parent.attach_filter}}">
                         <li class="services__sub-list-item">
-                            <a class="services__item-link" href="{{lnk_text}}">{{lnk_label}}</a>
+                            <a class="services__item-link" href="{{_parent._parent.url}}/{{id}}/{{wbFurlGenerate({{name}})}}">{{name}}</a>
                         </li>
                         </wb-foreach>
                     </ul>
@@ -25,6 +30,7 @@
                         <use xlink:href="/assets/img/sprite.svg#strategy"></use>
                     </svg>
                 </li>
+                </wb-data>
             </wb-foreach>
         </ol>
 
@@ -35,34 +41,14 @@
     <div>
         <wb-module wb="module=yonger&mode=edit&block=common.inc" />
     </div>
+    <div class="alert alert-info">
+        Список услуг формируется автоматически на основе данных раздела Услуги.
+    </div>
     <div class="form-group row">
         <label class="col-sm-3">Заголовок</label>
         <div class="col-sm-9">
             <input class="form-control" type="text" name="title" placeholder="Заголовок">
         </div>
     </div>
-    <wb-multiinput name="list">
-        <div class="col-12">
-            <div class="form-group row">
-                <label class="col-sm-3">Заголовок блока</label>
-                <div class="col-sm-4">
-                    <input class="form-control" type="text" name="blk_title" placeholder="Заголовок блока">
-                </div>
-                <div class="col-sm-5">
-                    <input class="form-control" type="text" name="blk_title_link" placeholder="Ссылка заголовка" wb="module=yonpageselect">
-                </div>
-                <label class="col-sm-3">Текст</label>
-                <div class="col-sm-9">
-                    <textarea class="form-control" rows="auto" name="blk_text" placeholder="Текст"></textarea>
-                </div>
-                <label class="col-sm-3">Ссылки</label>
-                <div class="col-sm-9">
-                    <wb-multiinput name="links">
-                        <input class="form-control col-sm-6 mb-1" name="lnk_label" placeholder="Текст ссылки">
-                        <input class="form-control col-sm-6 mb-1" name="lnk_text" placeholder="Ссылка" wb="module=yonpageselect" wb-url="/services">
-                    </wb-multiinput>
-                </div>
-            </div>
-        </div>
-    </wb-multiinput>
+
 </edit>
