b25b6e0a51fe937493c1ca1ae9fac412a8fec10a
Oleg Frolov <oleg_frolov@mail.ru>
2022-02-01 17:10:27 +0300

    update

--- blocks/list-service-category.php
+++ blocks/list-service-category.php
@@ -6,14 +6,14 @@
         </div>
 
         <ol class="services-section__list">
-            <wb-foreach wb="table=services&tpl=false&sort=name" wb-filter="{'active':'on','id':{'$ne': '{{_parent.id}}' }}">
+            <wb-foreach wb="table=pages&tpl=false&sort=name" wb-filter="{'active':'on','path':'\/services'}">
                 <li class="services-section__item">
                     <article class="service">
-                        <h3 class="service__title">{{name}}</h3>
+                        <h3 class="service__title">{{blocks.block_paragraph.title}}</h3>
                         <svg class="service__icon" width="180" height="60" aria-hidden="true">
                             <use xlink:href="/assets/img/sprite.svg#{{sprite}}"></use>
                         </svg>
-                        <p class="service__description">{{descr}}</p>
+                        <p class="service__description">{{blocks.block_paragraph.text}}</p>
                         <a class="service__link" href="/services/{{_id}}/{{wbFurlGenerate({{name}})}}">Подробнее</a>
                     </article>
                 </li>
@@ -21,11 +21,11 @@
         </ol>
     </section>
 </view>
-<edit header="Главная - услуги">
+<edit header="Список категорий услуг">
     <div>
         <wb-module wb="module=yonger&amp;mode=edit&amp;block=common.inc"></wb-module>
     </div>
     <div class="alert alert-info">
-        Контент блока генерируется автоматически на основе существующих проектов.
+        Контент блока генерируется автоматически на основе существующих страниц.
     </div>
 </edit>
