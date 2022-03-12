d3b6a269cb8cbc314e6fb86c1ae1a1b1fb65d404
Ildar7 <ildarvaliev167@gmail.com>
2022-03-12 21:14:39 +0300

    changes in service

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -469,190 +469,6 @@ function setThemeButton() {
     }
 }
 
-// function setFeedbackSlider() {
-//     const $slider = document.querySelector('.js-feedback-slider');
-
-//     if ($slider) {
-//         const activeSlides = 1;
-//         let $sliderList = $slider.querySelector('.js-feedback-slider-list');
-
-//         let isTransitionActive;
-//         let slideWidth = $sliderList.children[1].getBoundingClientRect().left - $sliderList.children[0].getBoundingClientRect().left;
-
-//         setActiveSlides($sliderList, activeSlides);
-
-//         window.addEventListener('resize', () => {
-//             slideWidth = $sliderList.children[1].getBoundingClientRect().left - $sliderList.children[0].getBoundingClientRect().left;
-//         });
-
-//         $slider.addEventListener('click', (evt) => {
-//             if (evt.target.closest('.js-feedback-slider-button--next')) {
-//                 next();
-//             }
-//         });
-
-//         $slider.addEventListener('transitionend', () => {
-//             if (isTransitionActive) {
-//                 removeSlide();
-//             }
-//         });
-
-//         /*eslint-disable no-inner-declarations */
-
-//         function next() {
-//             isTransitionActive = true;
-//             const nextSlide = $sliderList.firstElementChild.cloneNode(true);
-//             nextSlide.classList.remove('active');
-
-//             $sliderList.append(nextSlide);
-//             $sliderList.style.transform = `translateX(-${slideWidth}px)`;
-
-//             $sliderList = $slider.querySelector('.js-feedback-slider-list');
-//             setActiveSlides($sliderList, activeSlides);
-//         }
-
-//         function removeSlide() {
-//             $sliderList.firstElementChild.remove();
-
-//             $sliderList.style.transition = 'none';
-//             $sliderList.style.transform = 'translateX(0)';
-//             isTransitionActive = false;
-//             setTimeout(() => {
-//                 $sliderList.style.transition = 'transform 0.5s';
-//             }, 50);
-
-//             $sliderList = $slider.querySelector('.js-feedback-slider-list');
-//             setActiveSlides($sliderList, activeSlides);
-//         }
-
-//         function setActiveSlides(list, number) {
-//             for (let i = 0; i < number; i++) {
-//                 list.children[i].classList.add('active');
-//             }
-//         }
-//     }
-// }
-
-// function setSlider() {
-//     const $slider = document.querySelector('.js-slider');
-
-//     if ($slider) {
-//         const activeSlides = window.innerWidth >= 860 ? 4 : 1;
-
-//         let $sliderList = $slider.querySelector('.js-slider-list');
-
-//         let direction;
-//         let clickPrev = prev;
-//         let clickNext = next;
-//         let slideWidth = $sliderList.children[1].getBoundingClientRect().left - $sliderList.children[0].getBoundingClientRect().left;
-
-//         setActiveSlides($sliderList, activeSlides);
-
-
-//         window.addEventListener('resize', () => {
-//             slideWidth = $sliderList.children[1].getBoundingClientRect().left - $sliderList.children[0].getBoundingClientRect().left;
-//             setActiveSlides($sliderList, activeSlides);
-//         });
-
-//         $slider.addEventListener('click', (evt) => {
-//             if (evt.target.closest('.js-slider-button--prev')) {
-//                 clickPrev();
-//             }
-//             if (evt.target.closest('.js-slider-button--next')) {
-//                 clickNext();
-//             }
-//         });
-
-//         $slider.addEventListener('transitionend', () => {
-//             if (direction) {
-//                 removeSlide();
-//             }
-//         });
-
-//         /*eslint-disable no-inner-declarations */
-
-//         function prev() {
-//             direction = 1;
-//             if (clickPrev === null) {
-//                 return;
-//             } else {
-//                 clickPrev = null;
-//                 $sliderList.style.transition = 'none';
-//                 $sliderList.style.transform = `translateX(-${slideWidth}px)`;
-
-//                 const nextSlide = $sliderList.lastElementChild.cloneNode(true);
-//                 nextSlide.classList.add('active');
-//                 $sliderList.prepend(nextSlide);
-//                 setActiveSlides($sliderList, activeSlides);
-
-
-//                 setTimeout(() => {
-//                     $sliderList.style.transition = '0.5s';
-//                     $sliderList.style.transform = 'translateX(0)';
-
-//                 }, 50);
-
-//                 $sliderList = $slider.querySelector('.js-slider-list');
-//             }
-//         }
-
-//         function next() {
-//             direction = -1;
-//             if (clickNext === null) {
-//                 return;
-//             } else {
-//                 const nextSlide = $sliderList.firstElementChild.cloneNode(true);
-//                 nextSlide.classList.remove('active');
-
-//                 $sliderList.append(nextSlide);
-//                 $sliderList.style.transform = `translateX(-${slideWidth}px)`;
-//                 setActiveSlides($sliderList, activeSlides);
-
-//                 $sliderList = $slider.querySelector('.js-slider-list');
-//                 clickNext = null;
-//             }
-//         }
-
-//         function removeSlide() {
-
-//             if (direction === -1) {
-//                 $sliderList.firstElementChild.remove();
-
-//                 $sliderList.style.transition = 'none';
-//                 $sliderList.style.transform = 'translateX(0)';
-//                 direction = undefined;
-//                 setTimeout(() => {
-//                     $sliderList.style.transition = '0.5s';
-//                     clickNext = next;
-//                 }, 50);
-
-//             } else if (direction === 1) {
-//                 $sliderList.lastElementChild.remove();
-//                 clickPrev = prev;
-//                 direction = undefined;
-//             }
-
-//             $sliderList = $slider.querySelector('.js-slider-list');
-//             setActiveSlides($sliderList, activeSlides);
-//         }
-
-//         function setActiveSlides(list, number) {
-//             if (window.innerWidth < 1700) {
-//                 for (let i = 0; i < list.childElementCount; i++) {
-//                     list.children[i].classList.add('active');
-//                 }
-//             } else {
-//                 for (let i = 0; i < list.childElementCount; i++) {
-//                     list.children[i].classList.remove('active');
-//                     if (i < number) {
-//                         list.children[i].classList.add('active');
-//                     }
-//                 }
-//             }
-//         }
-//     }
-// }
-
 new Swiper('.brand__slider', {
     slidesPerGroup: 1,
     loop: true,
@@ -768,74 +584,6 @@ function setPopupSuccess() {
     formPopupContent.append(button);
 }
 
-// function init() {
-//     new SmoothScroll(document, 100, 5)
-// }
-
-// function SmoothScroll(target, speed, smooth) {
-//     if (target === document)
-//         target = (document.scrollingElement
-//             || document.documentElement
-//             || document.body.parentNode
-//             || document.body) // cross browser support for document scrolling
-
-//     var moving = false
-//     var pos = target.scrollTop
-//     var frame = target === document.body
-//         && document.documentElement
-//         ? document.documentElement
-//         : target // safari is the new IE
-
-//     target.addEventListener('mousewheel', scrolled, { passive: false })
-//     target.addEventListener('DOMMouseScroll', scrolled, { passive: false })
-
-//     function scrolled(e) {
-//         e.preventDefault(); // disable default scrolling
-
-//         var delta = normalizeWheelDelta(e)
-
-//         pos += -delta * speed
-//         pos = Math.max(0, Math.min(pos, target.scrollHeight - frame.clientHeight)) // limit scrolling
-
-//         if (!moving) update()
-//     }
-
-//     function normalizeWheelDelta(e) {
-//         if (e.detail) {
-//             if (e.wheelDelta)
-//                 return e.wheelDelta / e.detail / 40 * (e.detail > 0 ? 1 : -1) // Opera
-//             else
-//                 return -e.detail / 3 // Firefox
-//         } else
-//             return e.wheelDelta / 120 // IE,Safari,Chrome
-//     }
-
-//     function update() {
-//         moving = true
-
-//         var delta = (pos - target.scrollTop) / smooth
-
-//         target.scrollTop += delta
-
-//         if (Math.abs(delta) > 0.5)
-//             requestFrame(update)
-//         else
-//             moving = false
-//     }
-
-//     var requestFrame = function () { // requestAnimationFrame cross browser
-//         return (
-//             window.requestAnimationFrame ||
-//             window.webkitRequestAnimationFrame ||
-//             window.mozRequestAnimationFrame ||
-//             window.oRequestAnimationFrame ||
-//             window.msRequestAnimationFrame ||
-//             function (func) {
-//                 window.setTimeout(func, 1000 / 50);
-//             }
-//         );
-//     }()
-// }
 
 /* viewport width */
 function viewport() {
@@ -851,6 +599,23 @@ function viewport() {
     }
 };
 /* viewport width */
+
+const body = document.querySelector('body')
+const serviceLink = document.createElement('div')
+serviceLink.className = 'service__link'
+serviceLink.innerHTML = 'Подробнее'
+body.append(serviceLink)
+
+const serviceBlock = document.querySelector('.services-section__list')
+
+serviceBlock.onmousemove = function (event) {
+    serviceLink.style.display = 'block';
+    if (event.offsetY > 10) serviceLink.style.top = event.pageY + 20 + 'px';
+    if (event.offsetX > 10) serviceLink.style.left = event.pageX + 20 + 'px';
+}
+serviceBlock.onmouseleave = function () {
+    serviceLink.style.display = 'none';
+}
 
 $(document).ready(function () {
     setModal('.modal', '.form-section', '.js-form-open', '.js-form-close');
--- tpl/services-category.inc.php
+++ tpl/services-category.inc.php
@@ -1,14 +1,14 @@
         <ol class="services-section__list">
             <wb-foreach wb="table=pages&tpl=false&sort=_sort" wb-filter="{'active':'on','path':'\/services'}">
-                <li class="services-section__item">
+                <a href="/services/{{wbFurlGenerate({{name}})}}" class="services-section__item">
                     <article class="service">
                         <h3 class="service__title">{{blocks.block_paragraph.title}}</h3>
                         <svg class="service__icon" width="180" height="60" aria-hidden="true">
                             <use xlink:href="/assets/img/sprite.svg#{{name}}"></use>
                         </svg> 
                         <p class="service__description">{{blocks.block_paragraph.text}}</p>
-                        <a class="service__link" href="/services/{{wbFurlGenerate({{name}})}}">Подробнее</a>
                     </article>
-                </li>
+                </a>
             </wb-foreach>
         </ol>
+        
