4d584832d96f46fb84018c747edda54ac5ac1afb
Ildar7 <ildarvaliev167@gmail.com>
2022-04-15 15:20:34 +0300

    changes in js

--- blocks/blog.php
+++ blocks/blog.php
@@ -137,6 +137,7 @@
             function changeLink() {
                 const moreBtn = document.querySelector('.page-link.more');
                 moreBtn.innerHTML = `<span class="text-icon">загрузить еще</span> <span class="button__icon-wrapper"><svg class="button__icon" width="24" height="24"><use xlink:href="/assets/img/sprite.svg#arrow-right"></use></svg></span>`
+                moreBtn.href = 'javascript:void(0)';
             }
             $("#articlesList").bind("DOMSubtreeModified",function(){
                 changeLink();
