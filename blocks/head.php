713e82f10cdb098c655f9837c6bda2703f3b3599
Oleg Frolov <oleg_frolov@mail.ru>
2022-10-21 14:35:48 +0300

    update

---
+++ blocks/head.php
@@ -0,0 +1,71 @@
+<edit header="Блок HEAD">
+    <div>
+        <wb-module wb="module=yonger&mode=edit&block=common.inc" />
+    </div>
+</edit>
+
+<view>
+    <head>
+        <base href="/" />
+        <title>
+            Айдис - агентство по созданию брендов и цифровых продуктов
+        </title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <meta name="keywords" content="Проведение исследований,аналитика,разработка позиционирования и стратегии бренда,Нейминг,регистрация ТЗ (торгового знака) в ФИПС,слоган,дескриптор,копирайт,Айдентика,логотип,фирменный стиль,дизайн носители,брендбук,Веб-сайты,лендинги,веб-сервисы,Буклеты,презентации,иллюстрации,видеоролики">
+        <meta name="description" content="Branding и digital решения для современных компаний. Предлагаем услуги по созданию брендов, веб-сайтов, презентаций, дизайна и видео.">
+
+        <link rel="stylesheet" href="/assets/styles/index.css">
+        <link rel="preload" href="/assets/fonts/GrandisExtended-Regular.woff2" as="font" crossorigin="anonymous">
+        <link rel="preload" href="/assets/fonts/GrandisExtended-Medium.woff2" as="font" crossorigin="anonymous">
+        <link rel="preload" href="/assets/fonts/GrandisExtended-Bold.woff2" as="font" crossorigin="anonymous">
+
+        <link rel="icon" href="/favicon.ico">
+        <link rel="icon" href="/assets/img/favicons/icon.svg" type="image/svg+xml">
+        <link rel="apple-touch-icon" href="/assets/img/favicons/apple-touch-icon.png">
+        <link rel="manifest" href="/assets/manifest.webmanifest">
+
+        <!-- Yandex.Metrika counter -->
+        <script type="text/javascript">
+            (function(m, e, t, r, i, k, a) {
+                m[i] = m[i] || function() {
+                    (m[i].a = m[i].a || []).push(arguments)
+                };
+                m[i].l = 1 * new Date();
+                k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(
+                    k, a)
+            })
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(17921479, "init", {
+                clickmap: true,
+                trackLinks: true,
+                accurateTrackBounce: true,
+                webvisor: true,
+                trackHash: true
+            });
+        </script>
+        <noscript>
+            <div>
+                <img src="https://mc.yandex.ru/watch/17921479" style="position:absolute; left:-9999px;" alt="" />
+            </div>
+        </noscript>
+        <!-- /Yandex.Metrika counter -->
+
+        <!-- Яндекс верификатор для вебмастера -->
+        <meta name="yandex-verification" content="c15ff6821fb31878" />
+        <!-- /Яндекс верификатор для вебмастера -->
+        <script type="text/javascript">
+            ! function() {
+                var t = document.createElement("script");
+                t.type = "text/javascript", t.async = !0, t.src = 'https://vk.com/js/api/openapi.js?169', t.onload =
+                    function() {
+                        VK.Retargeting.Init("VK-RTRG-1363797-bWJc7"), VK.Retargeting.Hit()
+                    }, document.head.appendChild(t)
+            }();
+        </script>
+        <noscript>
+            <img src="https://vk.com/rtrg?p=VK-RTRG-1363797-bWJc7" style="position:fixed; left:-999px;" alt="" />
+        </noscript>
+    </head>
+</view>
