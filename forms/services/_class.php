c23d48aad4b25edbb2e16a13c42ae3617ed67f89
Oleg Frolov <oleg_frolov@mail.ru>
2022-11-14 13:39:20 +0300

    update

--- forms/services/_class.php
+++ forms/services/_class.php
@@ -1,6 +1,12 @@
 <?php
 class servicesClass extends cmsFormsClass
 {
+   
+    function beforeItemShow(&$item)
+    {
+        $item['header'] = $item['name'];
+    }
+
     function afterItemSave(&$item) {
         $this->app->shadow('/cms/ajax/form/pages/list');
     }
