a68bf54afffc1bc71f9c31fdf7d477c6416d908d
Oleg Frolov <oleg_frolov@mail.ru>
2022-11-17 18:28:49 +0300

    blog fix

--- forms/blog/_class.php
+++ forms/blog/_class.php
@@ -26,11 +26,11 @@ class blogClass extends cmsFormsClass
     function similar(&$item = null) {
         if (!$item) return;
         $list = $this->app->itemList('blog')['list'];
-        $tags = $item['tags'];
+        $tags = (array)$item['tags'];
         $similar = [];
         foreach($list as $art) {
             if ($art !== $item) {
-                $atags = $art['tags'];
+                $atags = (array)$art['tags'];
                 $ints = array_intersect($tags,$atags);
                 if (count($ints)) {
                     $similar[] = $art;
