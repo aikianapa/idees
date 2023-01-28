712a55d57a669ce1a83cb60029427680d96a4eed
Vitali <v.matyunin@jetmail.cc>
2023-01-28 16:09:58 +0300

    Блок услуг

--- blocks/block-services-list.php
+++ blocks/block-services-list.php
@@ -5,33 +5,47 @@
 </edit>
 
 <view>
+    <div class="gray-box2-clear section-outer">
+        <div class="container">
 
-    <div class="container">
-        <h2 class="sub-title"> Все услуги </h2>
-
-        <wb-var menu="{{yongerSiteMenu()}}" />
-
-        <wb-foreach wb="from=_var.menu&tpl=false">
-
-            <div class="uslugi" wb-if="'{{name}}'=='services'">
-
-                <wb-foreach wb="from=children&tpl=false">
-                    <div class="uslugi-item" wb-if="'{{name}}'!=='services'">
-                        <h3 class="sub-sub-title"> {{menu_title}} </h3>
-
-                        <div class="ttext">
-                        </div>
-                        <div class="tlist">
-                            <wb-foreach wb="table={{attach}}&tpl=false" wb-filter="{{attach_filter}}">
-                                <a href="{{path}}" class="menu__sub-link">
-                                    {{header}}
-                                </a>
-                            </wb-foreach>
-                        </div>
+            <div class="title-section">
+                <h2 class="title-section__title">Услуги агенства</h2>
+                <a href="#" class="title-section__link big-link">
+                    <div class="big-link__wrapper">
+                        <span class="big-link__text">Все <span class="big-link__text-all">услуги</span></span>
+                        <i class="big-link__icon-arrow">
+                            <svg class="big-link__icon-arrow-svg" width="34" height="9" aria-hidden="true">
+                                <use xlink:href="/assets/img/sprite.svg#big-arrow"></use>
+                            </svg>
+                        </i>
                     </div>
-                </wb-foreach>
+                </a>
             </div>
-        </wb-foreach>
+
+            <wb-var menu="{{yongerSiteMenu()}}" />
+
+            <wb-foreach wb="from=_var.menu&tpl=false">
+
+                <div class="uslugi" wb-if="'{{name}}'=='services'">
+
+                    <wb-foreach wb="from=children&tpl=false">
+                        <div class="uslugi-item" wb-if="'{{name}}'!=='services'">
+                            <h3 class="sub-sub-title"> {{menu_title}} </h3>
+
+                            <div class="ttext">
+                            </div>
+                            <div class="tlist">
+                                <wb-foreach wb="table={{attach}}&tpl=false" wb-filter="{{attach_filter}}">
+                                    <a href="{{path}}" class="menu__sub-link">
+                                        {{header}}
+                                    </a>
+                                </wb-foreach>
+                            </div>
+                        </div>
+                    </wb-foreach>
+                </div>
+            </wb-foreach>
+        </div>
     </div>
 
 </view>
