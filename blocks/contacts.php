4b06f46433b86306206aec913b1a47556624ede4
Oleg Frolov <oleg_frolov@mail.ru>
2023-02-13 09:42:04 +0300

    update

--- blocks/contacts.php
+++ blocks/contacts.php
@@ -1,22 +1,29 @@
-<view><section class="contacts__section container">
-  <h1 class="page__title">Контакты</h1>
+<view>
+  <section class="container contacts__section">
+    <h1 class="page__title">Контакты</h1>
 
-  <div class="contacts__container">
-    <a class="contacts__link" href="mailto:%7B%7B_var.email%7D%7D">{{_var.email}}</a>
-    <a class="contacts__link" href="tel:+%7B%7BwbDigitsOnly(%7B%7B_var.phone%7D%7D)%7D%7D">{{_var.phone}}</a>
+    <div class="contacts__container">
+      <a class="contacts__link" href="mailto:{{_var.email}}">{{_var.email}}</a>
+      <a class="contacts__link" href="tel:+{{wbDigitsOnly({{_var.phone}})}}">{{_var.phone}}</a>
 
-    <ul class="contacts__socials-list">
-      <li class="contacts__socials-item">
-        <a class="contacts__social-link" href="%7B%7B_var.whatsapp%7D%7D">Whatsapp</a>
-      </li>
-      <li class="contacts__socials-item">
-        <a class="contacts__social-link" href="%7B%7B_var.telegram%7D%7D">Telegram</a>
-      </li>
+      <ul class="contacts__socials-list">
+        <li class="contacts__socials-item">
+          <a class="contacts__social-link" href="{{_var.whatsapp}}">Whatsapp</a>
+        </li>
+        <li class="contacts__socials-item">
+          <a class="contacts__social-link" href="{{_var.telegram}}">Telegram</a>
+        </li>
+        <li class="contacts__socials-item">
+          <a class="contacts__social-link" href="{{_var.vkontakte}}">Vkontakte</a>
+        </li>
+      </ul>
 
-    </ul>
-
-    <address class="contacts__address">{{_var.address}}</address>
+      <address class="contacts__address">{{_var.address}}</address>
+    </div>
+  </section>
+</view>
+<edit header="Блок контактов">
+  <div>
+    <wb-module wb="module=yonger&amp;mode=edit&amp;block=common.inc"></wb-module>
   </div>
-</section></view><edit header="Блок контактов"><div>
-        <wb-module wb="module=yonger&amp;mode=edit&amp;block=common.inc"></wb-module>
-    </div></edit>
+</edit>
