4866f824f62ac4a195a3f78ffa5a4af2cde1087d
Oleg Frolov <oleg_frolov@mail.ru>
2023-02-13 14:57:57 +0300

    update

--- forms/projects/list.php
+++ forms/projects/list.php
@@ -1,8 +1,8 @@
 <html>
 <div class="m-3" id="yongerSpace">
     <nav class="nav navbar navbar-expand-md col">
-        <h3 class="tx-bold tx-spacing--2 order-1">Проекты</h3>
-        <div class="ml-auto order-2 float-right">
+        <h3 class="order-1 tx-bold tx-spacing--2">Проекты</h3>
+        <div class="order-2 float-right ml-auto">
             <a href="#" data-ajax="{'url':'/cms/ajax/form/projects/edit/_new','append':'modals'}"
                 class="btn btn-primary">
                 <img src="/module/myicons/24/FFFFFF/item-select-plus-add.svg" width="24" height="24" /> Добавить проект
@@ -10,9 +10,9 @@
         </div>
     </nav>
 
-    <div class="yonger-nested mb-1">
+    <div class="mb-1 yonger-nested">
         <span class="bg-light">
-            <div class="header p-2">
+            <div class="p-2 header">
                 <span clsss="row">
                     <div class="col-3">
                     <input class="form-control" type="search" placeholder="Поиск..."
@@ -28,7 +28,7 @@
                 <tr>
                     <td>Наименование</td>
                     <td>Теги</td>
-                    <td class="text-center">На главной</td>
+                    <!--td class="text-center">На главной</!--td-->
                     <td class="text-right">Действия</td>
                 </tr>
             </thead>
@@ -41,23 +41,23 @@
                             'filter': {'_site':'{{_sett.site}}'}
                 }">
                     <tr class="bg-transparent">
-                        <td  class="w-25 cursor-pointer" data-ajax="{'url':'/cms/ajax/form/projects/edit/{{_id}}','append':'modals'}" class="cursor-pointer">
+                        <td  class="cursor-pointer w-25" data-ajax="{'url':'/cms/ajax/form/projects/edit/{{_id}}','append':'modals'}" class="cursor-pointer">
                             {{name}}
                         </td>
                         <td class="w-25">
                             <wb-foreach wb-from="tags" wb-tpl="false">
-                                <small class="d-flex-inline p-1 mr-1 bg-light text-dark"
+                                <small class="p-1 mr-1 d-flex-inline bg-light text-dark"
                                     wb-if="'{{_val}}'>''">{{_val}}</small>
                             </wb-foreach>
                         </td>
-                        <td class="text-center">
+                        <!--td class="text-center">
                             <div class="custom-control custom-switch d-inline">
                                 <input type="checkbox" class="custom-control-input" name="home"
                                     id="projectsSwitchItemHome{{_idx}}"
                                     onchange="wbapp.save($(this),{'table':'projects','id':'{{_id}}','field':'home','silent':true})">
                                 <label class="custom-control-label" for="projectsSwitchItemHome{{_idx}}">&nbsp;</label>
                             </div>
-                        </td>
+                        </!--td-->
                         <td class="text-right nobr">
                             <div class="custom-control custom-switch d-inline">
                                 <input wb="module=swico" name="active"  onchange="wbapp.save($(this),{'table':'projects','id':'{{_id}}','field':'active','silent':true})">
