0d4436b0d551c1f847b830b6e338df429bea2728
Олег Фролов <oleg_frolov@mail.ru>
2023-07-15 13:07:07 +0300

    projects

--- blocks/projects.php
+++ blocks/projects.php
@@ -33,8 +33,9 @@
                         <li class="cases-new__case">
                             <a class="cases-new__img-link lazy-wrapper ttext" href="/projects/{{wbFurlGenerate({{name}})}}">
                                 <div class="cases-new__img-wrapper">
-                                    <img class="cases-new__img loading lazy-wrapper__img" width="790" height="500"
-                                        data-src="/thumbc/790x500/src/{{_var.cover}}"
+                                    <img class="cases-new__img loading lazy-wrapper__img" 
+                                    sizes="(max-width: 1200px) 1x, 2x"
+                                        data-src="/thumbc/790x500/src/{{_var.cover}} 1x"
                                         data-srcset="/thumbc/1580x1000/src/{{_var.cover}} 2x"
                                         alt="{{name}}">
                                 </div>
